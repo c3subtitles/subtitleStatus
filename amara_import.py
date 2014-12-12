30c578ce14a42723b6f91bb5056da34fa1ed3a0d
percidae <barbara@selfnet.de>
2014-12-12 06:40:05 +0100

    Script works

--- amara_import.py
+++ amara_import.py
@@ -34,10 +34,35 @@ for any_talk in all_talks_with_amara_key:
     amara_answer = json.loads(response.read().decode(encoding))
     
     # Number of available subtitle languages
-    available_subtitles = amara_answer["meta"]["total_count"]
-    print("Menge Untertitel: ",available_subtitles)
+    number_of_available_subtitles = amara_answer["meta"]["total_count"]
+    print("Menge Untertitel: ",number_of_available_subtitles)
     print("Laenge: ",len(amara_answer))
     print()
+    
+    # Get necessary info from json file for one subtitle
+    subtitles_counter = 0
+    while subtitles_counter < number_of_available_subtitles:
+        amara_language_code = amara_answer["objects"][subtitles_counter]["language_code"]
+        amara_is_original = amara_answer["objects"][subtitles_counter]["is_original"]
+        amara_num_versions = amara_answer["objects"][subtitles_counter]["num_versions"]
+        amara_subtitles_complete = amara_answer["objects"][subtitles_counter]["subtitles_complete"]
+        
+        print("language_code: ",amara_language_code,type(amara_language_code))
+        print("is_original: ",amara_is_original,type(amara_is_original))
+        print("num_versions: ",amara_num_versions,type(amara_num_versions))
+        print("subtitles_complete: ",amara_subtitles_complete,type(amara_subtitles_complete))
+        
+        # Ignore Subtitles with no saved revision
+        if amara_num_versions > 0:
+            language = Language.objects.get(lang_amara_short = amara_language_code)
+            subtitle = Subtitle.objects.get_or_create(language = language, talk = any_talk)[0]
+            print(type(subtitle.is_original_lang))
+            subtitle.is_original_lang = amara_is_original
+            subtitle.revision = amara_num_versions
+            subtitle.complete = amara_subtitles_complete
+            subtitle.save()
+        
+        subtitles_counter += 1
     """
     amara_answer = request.urlopen(url).read()
     print(type(amara_answer),"test")

032bb2a36f1710c565733f4fc4f5c10cc1dd0a0b
percidae <root@localhost>
2014-12-16 15:24:20 +0100

    Scrpit to only force update of 31c3 fahrplan

---
+++ force_31c3_update.py
@@ -0,0 +1,33 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# This script removes all schedule versions from the database which causes the
+# schedule update script to import every schedule again
+#==============================================================================
+
+import os
+import sys
+from lxml import etree
+from urllib import request
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtitleStatus.settings")
+
+import django
+django.setup()
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
+
+from www.models import Event
+
+try:
+    event_31c3 = Event.objects.get(acronym = "31c3")
+    event_31c3.schedule_version = "force update"
+    event_31c3.save()
+except:
+    print("Fehler")
+    
+print("Fahrplanversion für 31c3 zurück gesetzt!")
+
+

7383d7d0e003aff8b3d498c30d752051b58eec24
percidae <barbara@selfnet.de>
2014-12-22 01:27:36 +0100

    Changed default values to unknown

--- www/models.py
+++ www/models.py
@@ -20,7 +20,7 @@ class Event(BasisModell):
     start = models.DateField(default = "1970-01-01", blank = True)
     end = models.DateField(default = "1970-01-01", blank = True)
     timeslot_duration = models.TimeField(default = "00:15", blank = True)
-    days = models.PositiveSmallIntegerField(default = 1, blank = True)        
+    days = models.PositiveSmallIntegerField(default = 1, blank = True)
     schedule_xml_link = models.URLField()
 
 
@@ -30,7 +30,7 @@ class Event(BasisModell):
             return savedXML == xmlFile.data
 
 
-# Tage die zu einem Event zugeordnet sind    
+# Tage die zu einem Event zugeordnet sind
 class Event_Days(BasisModell):
     event = models.ForeignKey(Event)
     index = models.PositiveSmallIntegerField(default = 0)
@@ -43,23 +43,35 @@ class Rooms(BasisModell):
     room = models.CharField(max_length = 30, default = "kein Raum")
     building = models.CharField(max_length = 30, default = "")
 
+    def __str__(self):
+        return self.room
+
 # Verschiedene Sprachen in verschiedenen Kodierungen sowie auf D und E ausgeschrieben
 class Language(BasisModell):
     language_en = models.CharField(max_length = 40, default = "")
-    language_de = models.CharField(max_length = 40, default = "", blank = True) 
+    language_de = models.CharField(max_length = 40, default = "", blank = True)
     lang_short_2 = models.CharField(max_length = 3, default = "", blank = True)#, unique = True)
     lang_amara_short = models.CharField(max_length = 15, default = "", unique = True)
     lang_short_srt = models.CharField(max_length = 15,default = "", blank = True)
     language_native = models.CharField(max_length = 40, default = "", blank = True)
     amara_order = models.PositiveSmallIntegerField(default=0, blank = True)
 
+    def __str__(self):
+        return self.lang_amara_short
+
 # Kategorie des Talks
 class Tracks(BasisModell):
     track = models.CharField(max_length = 50, default = "")
 
+    def __str__(self):
+        return self.track
+
 # Präsentationsform des Talks
 class Type_of(BasisModell):
     type = models.CharField(max_length = 20, default = "")
+
+    def __str__(self):
+        return self.type
 
 # Vortragender
 class Speaker(BasisModell):
@@ -79,8 +91,8 @@ class Talk(BasisModell):
     title = models.CharField(max_length = 100, default = "ohne Titel", blank = True)
     subtitle_talk = models.CharField(max_length = 300, default = " ", blank = True) # nicht UT sondern Ergänzung zum Titel
     track = models.ForeignKey(Tracks, default = 40, blank = True)
-    event = models.ForeignKey(Event, default = "0", blank = True)
-    type_of = models.ForeignKey(Type_of, default = 6, blank = True)
+    event = models.ForeignKey(Event, default = 3, blank = True)
+    type_of = models.ForeignKey(Type_of, default = 9, blank = True)
     orig_language = models.ForeignKey(Language, default = 287, blank = True)
     abstract = models.TextField(default = "", blank = True)
     description = models.TextField(default = "", blank = True)
@@ -98,6 +110,8 @@ class Talk(BasisModell):
 class States(BasisModell):
     state_de = models.CharField(max_length = 100)
     state_en = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.state_de
 
 # Infos zu einem Untertitel in einer Sprache
 class Subtitle(BasisModell):
@@ -109,11 +123,12 @@ class Subtitle(BasisModell):
     state = models.ForeignKey(States, default = 1, blank = True)
     time_processed_transcribing = models.TimeField(default = "00:00", blank = True)
     time_processed_syncing = models.TimeField(default = "00:00", blank = True)
+    time_quality_check_done = models.TimeField(default = "00:00", blank = True)
     time_processed_translating = models.TimeField(default = "00:00", blank = True)
     #comment = models.TextField(default = "")
-    
+
 # Links aus dem Fahrplan
 class Links(BasisModell):
     talk = models.ForeignKey(Talk, blank = True)
     url = models.URLField(blank = True)
-    title = models.CharField(max_length = 200, default = "Link title", blank = True)   
+    title = models.CharField(max_length = 200, default = "Link title", blank = True)

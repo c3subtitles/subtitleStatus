08704287b767d5033a92146a80c4d85a3229c4dc
percidae <barbara@selfnet.de>
2015-01-02 05:16:30 +0100

    minor changes

--- import_and_convert_srt_file_from_amara_for_youtube.py
+++ import_and_convert_srt_file_from_amara_for_youtube.py
@@ -2,18 +2,25 @@
 # -*- coding: utf-8 -*-
 
 #==============================================================================
-#
-#
+# This scripts checks for the flag "needs_automatic_syncing" in the database
+# If a subtitle has that flag it downloads it and converts it to a pure
+# transcript
+# Afterwards it sends the file as attachment to an email address and resets
+# the flag in the database
 #==============================================================================
 
 import os
 import sys
 import urllib
-import smtplib
-import smtpd
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
 import re
+
+# E-Mail-Stuff
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+#from email.mime.image import MIMEImage
+from email import encoders
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtitleStatus.settings")
 
@@ -28,7 +35,8 @@ from www.models import Talk, Language, Subtitle
 # Search for subtitles with set flag "needs_automatic_syncing"
 my_subtitles = Subtitle.objects.filter(needs_automatic_syncing = True)
 
-TO = "barbara@selfnet.de"
+FROM = "localhost@subtitles.ber.c3voc.de"
+TO = ""
 TEXT = []
 TEXT.append("These Subtitle-Files need your attention: ")
 
@@ -71,25 +79,39 @@ for any in my_subtitles:
     filename = slug+"."+str(language)+".transcript"
     folder = "./downloads/"
     
+    # Save File in ./downloads
     file = open(folder+filename,mode = "w",encoding = "utf-8")
     for line in transcript:
         line = re.sub("<i>","*",line)
         line = re.sub("</i>","*",line)
         file.write(line)
     file.close()
-    TEXT.append(filename)
-   
-    SUBJECT = str(len(my_subtitles))+" Transcripts need your attention!"
-
-    COMMASPACE = ', '
-    msg = MIMEMultipart()
-    msg["Subject"] = SUBJECT
-    ME = "percidae@localhost"
-    msg["From"] = ME
-    msg["To"] = TO
-    msg.preamble = SUBJECT
     
-#    s = smtplib.SMTP(ME,TO,msg.as_string())
-#    s.quit()
-#    s.sendmail()
-
+    # Building the email
+    msg = MIMEMultipart()
+    filename = folder+filename
+    #print(filename)
+    
+    # Build attachment File for email an attach
+    attachment = MIMEBase('application', 'octet-stream')
+    attachment.set_payload(open(filename, 'rb').read())
+    encoders.encode_base64(attachment)
+    attachment.add_header('Content-Disposition', 'attachment',filename=os.path.split(filename)[1])
+    msg.attach(attachment)
+    
+    # Build text for email with important Links
+    text = MIMEText("Talk: "+any.talk.title+"\n"+
+        "Bitte auf Youtube laden und syncen lassen!\n\n"+
+        "Video-Adresse: "+any.talk.link_to_video_file+"\n"+
+        "Amara-Adresse: "+"www.amara.org/videos/"+any.talk.amara_key+"/", "plain")
+    msg.attach(text)
+    msg["Subject"] = "Transcript needs your attention: "+str(any.talk.frab_id_talk)+' "'+any.talk.title+'"'
+    msg["From"] = FROM
+    msg["To"] = TO
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+    any.needs_automatic_syncing = False
+    any.save()
+    
+    

1ecbcfeb28dd24549dbabfc879d92d0ee4f95ff3
percidae <root@localhost>
2015-01-17 22:31:54 +0100

    Bugfixes

--- www/forms.py
+++ www/forms.py
@@ -4,7 +4,7 @@ from .models import Subtitle
 class SubtitleForm(forms.ModelForm):
     class Meta:
         model = Subtitle
-        fields = ['state', 'time_processed_transcribing', 'time_processed_syncing', 'time_quality_check_done', 'time_processed_translating']
+        fields = ['time_processed_transcribing', 'time_processed_syncing', 'time_quality_check_done', 'time_processed_translating']
 
     def clean(self):
         cleaned_data = super(SubtitleForm, self).clean()
@@ -16,7 +16,6 @@ class SubtitleForm(forms.ModelForm):
 
         if cleaned_data['time_processed_transcribing'] > my_obj.talk.video_duration:
            self._errors['time_processed_transcribing'] = self.error_class(['Time longer than the talk.'])
-        print(cleaned_data)
         if cleaned_data['time_processed_syncing'] > my_obj.talk.video_duration:
            self._errors['time_processed_syncing'] = self.error_class(['Time longer than the talk.'])
         if cleaned_data['time_quality_check_done'] > my_obj.talk.video_duration:

e18731480a072a555113689f2f5af4a0e4825157
percidae <barbara@selfnet.de>
2016-01-06 21:34:22 +0100

    Added setting the flags for a possible releases-tweet-function

--- update_subtitles_via_amara_import.py
+++ update_subtitles_via_amara_import.py
@@ -88,11 +88,13 @@ for any_talk in all_talks_with_amara_key:
                     subtitle.time_processed_syncing = subtitle.talk.video_duration
                     subtitle.time_quality_check_done = subtitle.talk.video_duration
                     subtitle.needs_sync_to_ftp = True
+                    subtitle.tweet = True
                 # If translation and finished set state to translation finished
                 elif (not subtitle.is_original_lang and subtitle.complete):
                     subtitle.state_id = 12
                     subtitle.time_processed_translating = subtitle.talk.video_duration
                     subtitle.needs_sync_to_ftp = True
+                    subtitle.tweet = True
                 # If translation and not finished set state to translation in progress    
                 elif (not subtitle.is_original_lang and not subtitle.complete):
                     # If the state was set to finished but isn't anymore, remove from ftp

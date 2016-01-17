d29dd5bdebf1605097259099cc21415829a07d7c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2016-01-18 00:06:37 +0100

    Implement auto-reloading on code changes
    Automatically reload the uwsgi processes on code changes.

--- subtitleStatus/wsgi.py
+++ subtitleStatus/wsgi.py
@@ -8,7 +8,17 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import uwsgi
+from uwsgidecorators import timer
+from django.utils import autoreload
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtitleStatus.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+@timer(3)
+def autoreload_on_code_change(_):
+    if autoreload.code_changed():
+        uwsgi.reload()

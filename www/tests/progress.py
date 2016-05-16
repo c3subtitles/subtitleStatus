cb14f82519cef6d57485eb275733a34404bd652a
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2016-05-16 04:53:28 +0200

    Add tests for progress bars

---
+++ www/tests/progress.py
@@ -0,0 +1,30 @@
+from math import fsum
+from .fixture import Fixture
+from ..views import progress_bar_for_talks
+
+class ProgressBarTestCase(Fixture):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.talks = cls.event.talk_set.filter(blacklisted=False)
+
+    def _check_bar(self, bar):
+        total = fsum(bar.values())
+        self.assertAlmostEqual(total, 100)
+
+        for value in bar.values():
+            self.assertTrue(0 <= value <= 100)
+
+    def _check_talks(self, talks):
+        self._check_bar(progress_bar_for_talks(talks))
+
+    def testTotal(self):
+        self._check_talks(self.talks)
+
+    def testTotalDay(self):
+        for day in self.days:
+            self._check_talks(self.talks.filter(day=day))
+
+    def testTotalLanguage(self):
+        for language in self.languages:
+            self._check_talks(self.talks.filter(orig_language=language))

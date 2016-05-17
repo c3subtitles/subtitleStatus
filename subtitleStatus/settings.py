e92b04729a8b00f424f7ab587a66cab9c0eadc7f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2016-05-18 00:53:07 +0200

    Add context processors

--- subtitleStatus/settings.py
+++ subtitleStatus/settings.py
@@ -20,7 +20,17 @@ PROJECT_ROOT = BASE_DIR
 TEMPLATES = [
     {'BACKEND': 'django.template.backends.django.DjangoTemplates',
      'APP_DIRS': True,
-     'OPTIONS': {'debug': False,},
+     'OPTIONS': {'debug': False,
+                 'context_processors': [
+                     'django.contrib.auth.context_processors.auth',
+                     'django.template.context_processors.debug',
+                     'django.template.context_processors.i18n',
+                     'django.template.context_processors.media',
+                     'django.template.context_processors.static',
+                     'django.template.context_processors.tz',
+                     'django.contrib.messages.context_processors.messages',
+                ],
+     },
     },
 ]
 

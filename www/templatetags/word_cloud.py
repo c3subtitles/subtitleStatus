32d197e9243e912ec3d09a8000b5adf99e3863af
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2016-12-11 23:33:52 +0100

    Add new tag for scaled word clouds

--- www/templatetags/word_cloud.py
+++ www/templatetags/word_cloud.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 from simplejson import dumps
+from random import randint
 
 register = template.Library()
 
@@ -24,3 +25,20 @@ def word_cloud(word_frequencies, element_id, autoescape=True):
     return mark_safe(render_to_string('www/word_cloud.html',
                                       {'words': wordlist,
                                        'element_id': element_id}))
+
+
+@register.inclusion_tag('www/scaled_word_cloud.html')
+def common_words_cloud(item, height='23em'):
+    """Generate a word cloud from any object having a `n_common_words`
+    property. A suitable HTML ID is generated from `item.id` if
+    applicable, otherwise an id is generated randomly.
+
+    The optional argument `height` will be used to scale the containing
+    `div` element."""
+
+    context = {'words': item.n_common_words,
+               'height': height,
+               'id': getattr(item, 'id', randint(0, 16384)),
+              }
+
+    return context

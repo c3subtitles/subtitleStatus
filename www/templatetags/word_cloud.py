215f31531a8576fbdfa21090d2d4dcbc2ed0a3f6
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2016-12-12 00:38:24 +0100

    Add whole-talk word clouds

--- www/templatetags/word_cloud.py
+++ www/templatetags/word_cloud.py
@@ -28,17 +28,27 @@ def word_cloud(word_frequencies, element_id, autoescape=True):
 
 
 @register.inclusion_tag('www/scaled_word_cloud.html')
-def common_words_cloud(item, height='23em'):
+def common_words_cloud(item, height='23em', property='n_common_words', id=None):
     """Generate a word cloud from any object having a `n_common_words`
     property. A suitable HTML ID is generated from `item.id` if
     applicable, otherwise an id is generated randomly.
 
     The optional argument `height` will be used to scale the containing
-    `div` element."""
+    `div` element.
 
-    context = {'words': item.n_common_words,
+    Optionally, an alternative property may be specified using the
+    `property` argument.
+
+    The `id` argument allows to specify a suffix used for
+    disambiguation of generated HTML IDs, for example to genarate
+    multiple clouds for the same `item`."""
+
+    context = {'words': getattr(item, property),
                'height': height,
                'id': getattr(item, 'id', randint(0, 16384)),
               }
+
+    if id is not None:
+        context['id'] = '%s-%s' % (context['id'], id)
 
     return context

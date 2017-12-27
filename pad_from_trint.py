176eac6318c3b772f3700602b36ef4f22f0cbd32
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2017-12-27 13:12:00 +0100

    Add script to convert to pad format from trint SRT

---
+++ pad_from_trint.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+
+def chunks(file):
+    """Read `file`, splitting it at doubled linebreaks"""
+    lines = []
+    for line in file:
+        lines.append(re.sub(' {2,}', ' ', line.strip()))
+    return '\n'.join(lines).split('\n\n')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit("usage: ./{} transcript.sbv".format(sys.argv[0]))
+
+    transcript = []
+    try:
+        with open(sys.argv[1], 'r') as file:
+            transcript = chunks(file)
+    except IOError as err:
+        sys.exit("Transcript not readable: {}").format(err)
+
+    out = ""
+
+    for chunk in transcript:
+        lines = chunk.split('\n')
+
+        for line in lines[2:]:
+            if line == '':
+                continue
+            out += ' ' + line
+
+    print(out.replace('\n\n', '\n'))

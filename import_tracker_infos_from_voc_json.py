9629083b60d87de7480ac9086e8f700db99b0467
percidae <barbara@selfnet.de>
2018-01-20 23:11:34 +0100

    URL changes and minor code changes

--- import_tracker_infos_from_voc_json.py
+++ import_tracker_infos_from_voc_json.py
@@ -22,7 +22,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from www.models import Talk
 
-url = "https://dl.jeising.net/misc/amara-videos.json"
+url = "https://dl.texec.de/projects/33c3/subtitles/amara-videos.json"
 
 request = urllib.request.Request(url)
 response = urllib.request.urlopen(request)
@@ -32,12 +32,28 @@ key_file = json.loads(response.read().decode(encoding))
 #print(key_file)
 
 for any in key_file:
-    print(any["fahrplan_guid"])
+    print(any["fahrplan_guid"], any["fahrplan_id"])
     try:
         this_talk = Talk.objects.get(frab_id_talk = any["fahrplan_id"], guid = any["fahrplan_guid"])
-        this_talk.amara_key = any["amara_id"]
-        this_talk.youtube_key = any["youtube_url"][-11:]
-        this_talk.link_to_video_file = any["youtube_url"]
-        #this_talk.save()
+        if this_talk.amara_key != any["amara_id"]:
+            this_talk.amara_key = any["amara_id"]
+            this_talk.save()
+            print("id %s updated amara_key " % this_talk.id)
+        if this_talk.youtube_key != any["youtube_url"][-11:]:
+            this_talk.youtube_key = any["youtube_url"][-11:]
+            this_talk.save()
+            print("id %s updated youtube_key " % this_talk.id)
+        if this_talk.link_to_video_file != any["youtube_url"]:
+            this_talk.link_to_video_file = any["youtube_url"]
+            this_talk.save()
+            print("id %s updated link_to_video_file " % this_talk.id)
+        if this_talk.youtube_key_t_1 != any["youtube_url_translated"][-11:]:
+            this_talk.youtube_key_t_1 = any["youtube_url_translated"][-11:]
+            this_talk.save()
+            print("id %s updated youtube_key_t_1" % this_talk.id)
+        if this_talk.youtube_key_t_2 != any["youtube_url_translated_2"][-11:]:
+            this_talk.youtube_key_t_2 = any["youtube_url_translated_2"][-11:]
+            this_talk.save()
+            print("id %s updated youtube_key_t_2" % this_talk.id)
     except:
         print("Entry with guid %s and frab_id %s failed!" % (any["fahrplan_guid"], any["fahrplan_id"]) )

519fc465598c3888227fae69b893701efafe6272
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2018-12-31 12:01:42 +0100

    Add pad-from-youtube action

--- pad_from_youtube.py
+++ pad_from_youtube.py
@@ -2,38 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import re
 
-def chunks(file):
-    """Read `file`, splitting it at doubled linebreaks"""
-    lines = []
-    for line in file:
-        lines.append(re.sub(' {2,}', ' ', line.strip()))
-    return '\n'.join(lines).split('\n\n')
+from www.transforms import pad_from_youtube
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit("usage: ./{} transcript.sbv".format(sys.argv[0]))
 
-    transcript = []
+    transcript = ''
     try:
         with open(sys.argv[1], 'r') as file:
-            transcript = chunks(file)
+            transcript = file.read()
     except IOError as err:
         sys.exit("Transcript not readable: {}").format(err)
 
-    out = ""
-
-    for chunk in transcript:
-        lines = chunk.split('\n')
-
-        for line in lines[1:]:
-            if line == '':
-                continue
-            if line[0] == '[' and line[-1] == ']':
-                 # keep this as a separate line
-                 out += '\n' + line + '\n'
-            else:
-                 out += ' ' + line
-
-    print(out.replace('\n\n', '\n'))
+    print(pad_from_youtube(transcript))

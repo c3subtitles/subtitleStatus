c18f361825278c924513836dbe1b3e618de06f3c
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2019-12-29 13:42:55 +0100

    Fix m2m talk.persons migration
    Django is not able to perform AlterField migrations that change
    ManyToMany fields:
    
    ValueError: Cannot alter field %s into %s - they are not compatible
    types (you cannot alter to or from M2M fields, or add or remove
    through= on M2M fields)
    
    A workaround is to pretend to create a new model for the relationship
    table and remove/add the field via the SeparateDatabaseAndState
    migration. A SQL migration is not needed, because no schema changes are
    required.

--- www/migrations/0112_auto_20161009_1725.py
+++ www/migrations/0112_auto_20161009_1725.py
@@ -31,25 +31,76 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
-        migrations.CreateModel(
-            name='Talk_Persons',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('touched', models.DateTimeField(auto_now=True)),
-                ('words', models.IntegerField(null=True, blank=True)),
-                ('strokes', models.IntegerField(null=True, blank=True)),
-                ('time_delta', models.FloatField(null=True, blank=True)),
-                ('average_wpm', models.FloatField(null=True, blank=True)),
-                ('average_spm', models.FloatField(null=True, blank=True)),
-                ('recalculate_statistics', models.BooleanField(default=False)),
-                ('speaker', models.ForeignKey(to='www.Speaker')),
-                ('talk', models.ForeignKey(to='www.Talk')),
+        migrations.SeparateDatabaseAndState(
+            None,
+            [
+                migrations.CreateModel(
+                    name='Talk_Persons',
+                    fields=[
+                        ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                        ('speaker', models.ForeignKey(to='www.Speaker')),
+                        ('talk', models.ForeignKey(to='www.Talk')),
+                    ],
+                    options={
+                        'abstract': False,
+                    },
+                    bases=(models.Model,),
+                ),
+                migrations.AddField(
+                    model_name='talk',
+                    name='persons_new',
+                    field=models.ManyToManyField(to='www.Speaker', through='www.Talk_Persons', default=None, blank=True),
+                ),
+                migrations.RemoveField(
+                    model_name='talk',
+                    name='persons',
+                ),
+                migrations.RenameField(
+                    model_name='talk',
+                    old_name='persons_new',
+                    new_name='persons'
+                ),
             ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='created',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='touched',
+            field=models.DateTimeField(auto_now=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='average_spm',
+            field=models.FloatField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='average_wpm',
+            field=models.FloatField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='recalculate_statistics',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='strokes',
+            field=models.IntegerField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='time_delta',
+            field=models.FloatField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='Talk_Persons',
+            name='words',
+            field=models.IntegerField(null=True, blank=True),
         ),
         migrations.RemoveField(
             model_name='statistics_raw_data',
@@ -90,10 +141,5 @@ class Migration(migrations.Migration):
             name='speakers_words',
             field=models.IntegerField(null=True, blank=True),
             preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='talk',
-            name='persons',
-            field=models.ManyToManyField(to='www.Speaker', through='www.Talk_Persons', default=None, blank=True),
         ),
     ]

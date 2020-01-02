a94c963b298a53ecd56fcc9409247da62ebeca7c
percidae <percidae@users.noreply.github.com>
2020-01-02 18:41:39 +0100

    Removed ftp_startfolder from class Event, not needed anymore

--- www/models.py
+++ www/models.py
@@ -39,7 +39,7 @@ class Event(BasisModell):
     schedule_xml_link = models.URLField()
     city = models.CharField(max_length = 30, default = "", blank = True)
     building = models.CharField(max_length = 30, default = "", blank = True)
-    ftp_startfolder = models.CharField(max_length = 100, default = "", blank = True)
+    #ftp_startfolder = models.CharField(max_length = 100, default = "", blank = True)
     ftp_subfolders_extensions = models.ManyToManyField(Folders_Extensions, default = None, blank = True)
     hashtag = models.CharField(max_length = 20, default = "", blank = True)
     subfolder_to_find_the_filenames = models.CharField(max_length = 20, default = "", blank = True) # To find the right filenames via regex via frab-id

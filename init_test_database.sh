c61b86fdf33829e9e37f64859153e26360c05f10
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-01-16 17:10:09 +0100

    Update test fixture

--- init_test_database.sh
+++ init_test_database.sh
@@ -4,6 +4,6 @@ mkdir -p www/static/fahrplan_files/
 python manage.py migrate
 python manage.py set_default_site --name localhost --domain localhost:8000
 python manage.py createsuperuser --username admin --email root@localhost
-python manage.py loaddata 32c3
+python manage.py loaddata dump-2021-01-16
 python import_languages.py
-python update_events_xml_schedule_import.py
+#python update_events_xml_schedule_import.py

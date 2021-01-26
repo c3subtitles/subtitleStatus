51c74dc745002300106aca3f69ade7b63ffe0702
percidae <percidae@users.noreply.github.com>
2021-01-26 16:19:06 +0100

    Added *.refresh_from_db() to avoid overwriting any changes which happened in the meantime

--- calculate_statistics_data.py
+++ calculate_statistics_data.py
@@ -25,37 +25,44 @@ from www.models import Statistics_Raw_Data, Talk, Talk_Persons, Statistics_Speak
 # Statistics_Raw_Data
 my_statistics = Statistics_Raw_Data.objects.filter(recalculate_statistics = True)
 for any_statistics in my_statistics:
+    any_statistics.refresh_from_db()
     any_statistics.recalculate()
 
 # Whole Talk Statistics
 my_statistics = Talk.objects.filter(recalculate_talk_statistics = True)
 for any_statistics in my_statistics:
+    any_statistics.refresh_from_db()
     any_statistics.recalculate_whole_talk_statistics()
 
 # Talk, only Speakers Time Statistics
 my_statistics = Talk.objects.filter(recalculate_speakers_statistics = True)
 for any_statistics in my_statistics:
+    any_statistics.refresh_from_db()
     any_statistics.recalculate_speakers_in_talk_statistics()
 
 # Create the entries for Statistics_Event if necessary, ignore #3
 all_events = Event.objects.all().exclude(id = 3)
 for any in all_events:
+    any.refresh_from_db()
     any.create_statistics_event_entries()
 
 # Event Statistics
 my_statistics = Statistics_Event.objects.filter(recalculate_statistics = True)
 for any_statistics in my_statistics:
+    any_statistics.refresh_from_db()
     any_statistics.recalculate()
 
 # Talk_Persons
 my_statistics = Talk_Persons.objects.filter(recalculate_statistics = True)
 for any_statistics in my_statistics:
+    any_statistics.refresh_from_db()
     any_statistics.recalculate()
 
 # Statistics Speakers
 my_statistics = Statistics_Speaker.objects.filter(recalculate_statistics = True)
 for any_statistics in my_statistics:
-    any_statistics.recalculate()    
+    any_statistics.refresh_from_db()
+    any_statistics.recalculate()
 
 print("Done!")
 

09aef808183c2ef3a964b3781aba587258ea82b1
percidae <barbara@wh-netz.de>
2021-02-19 17:06:19 +0100

    Added the status 'released_as_draft' and changed the filename according to the state

--- www/views.py
+++ www/views.py
@@ -515,9 +515,13 @@ def media_export(request, timestamp, *argh, **kwargs):
     activity_data = {}
 
     csv_output = ""
-    csv_output += ("GUID;complete;media_language;srt_language;last_changed_on_amara;revision;url;touched;amara_key;amara_language;state;amara_subtitle_url\n")
+    csv_output += ("GUID;complete;media_language;srt_language;last_changed_on_amara;revision;url;touched;amara_key;amara_language;state;amara_subtitle_url;released_as_draft\n")
     for any in my_subtitles:
-        csv_output += any.talk.guid+";"+str(any.complete)+";"+any.language.lang_code_media+";"+any.language.lang_short_srt+";"+any.last_changed_on_amara.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+str(any.revision)+";https://mirror.selfnet.de/c3subtitles/"+any.talk.event.subfolder_in_sync_folder+"/"+any.talk.filename+"."+any.language.lang_short_srt+".srt"+";"+any.touched.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+any.talk.amara_key+";"+any.language.lang_amara_short+";"+str(any.state_id)+";"+"https://amara.org/api/videos/"+any.talk.amara_key+"/languages/"+any.language.lang_amara_short +"/subtitles/"+"\n"
+        # If the Subtitle is in quality control state and a draft is released
+        if any.state_id == 7:
+            csv_output += any.talk.guid+";"+str(any.complete)+";"+any.language.lang_code_media+";"+any.language.lang_short_srt+";"+any.last_changed_on_amara.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+str(any.revision)+";https://mirror.selfnet.de/c3subtitles/"+any.talk.event.subfolder_in_sync_folder+"/"+any.talk.filename+"."+any.language.lang_short_srt+ "_DRAFT" + ".srt"+";"+any.touched.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+any.talk.amara_key+";"+any.language.lang_amara_short+";"+str(any.state_id)+";"+"https://amara.org/api/videos/"+any.talk.amara_key+"/languages/"+any.language.lang_amara_short +"/subtitles/"+ ";" + str(True) + "\n"
+        else:
+            csv_output += any.talk.guid+";"+str(any.complete)+";"+any.language.lang_code_media+";"+any.language.lang_short_srt+";"+any.last_changed_on_amara.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+str(any.revision)+";https://mirror.selfnet.de/c3subtitles/"+any.talk.event.subfolder_in_sync_folder+"/"+any.talk.filename+"."+any.language.lang_short_srt+".srt"+";"+any.touched.strftime("%Y-%m-%dT%H:%M:%SZ")+";"+any.talk.amara_key+";"+any.language.lang_amara_short+";"+str(any.state_id)+";"+"https://amara.org/api/videos/"+any.talk.amara_key+"/languages/"+any.language.lang_amara_short +"/subtitles/"+ ";" + str(False) + "\n"
 
     #return render(request, 'www/b_test.html', {"data":data})
     #return render(request, "www/raw_csv.html", {"data":csv_output})

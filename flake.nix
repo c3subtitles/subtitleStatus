cfaf3dba835f8daf3f43af4a44c1b5bf7f21a710
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-12-27 23:04:11 +0100

    Upgrade flake to use poetry2nix

--- flake.nix
+++ flake.nix
@@ -8,75 +8,33 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = import ./nix;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        };
-      in
-      {
-        inherit overlay;
-        devShell = pkgs.mkShell
-          {
-            SUBTITLESTATUS_CONFIG = "${pkgs.subtitleStatusCfg}";
-
-            buildInputs = [
-              (pkgs.python3.withPackages (pyPkgs: with pyPkgs;
-              [
-                django
-                pkgs.django-account
-                pkgs.django-bootstrap-form
-                pkgs.django-extensions
-                django-pglocks
-                factory_boy
-                faker
-                google-api-python-client
-                google-auth
-                google-auth-httplib2
-                httplib2
-                isodate
-                pkgs.python-kanboard-api
-                lxml
-                oauth2client
-                paramiko
-                (psycopg2.overrideAttrs (drv: rec {
-                  version = "2.8.6";
-                  src = pyPkgs.fetchPypi {
-                    inherit version;
-                    inherit (drv) pname;
-                    sha256 = "sha256-+yP2xxEHw3/WZ8tOo2Pd65NrNIu9ZEknjrksGJaZ9UM=";
-                  };
-                }))
-                pyasn1
-                pyasn1-modules
-                pynacl
-                pysftp
-                python-dateutil
-                pytz
-                requests
-                rsa
-                simplejson
-                six
-                sqlparse
-                text-unidecode
-                twitter
-                uritemplate
-                urllib3
-                (pkgs.uwsgi.override
-                  {
-                    plugins = [ "python3" ];
-                  })
-                xmltodict
-                pip
-              ]))
-
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlay = import ./nix;
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              overlay
+              poetry2nix.overlay
             ];
           };
-      });
-
+        in
+        {
+          inherit overlay;
+          devShell =
+            let
+              poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+                projectDir = ./.;
+              };
+            in
+            poetryEnv.env.overrideAttrs
+              (oldAttrs: {
+                SUBTITLESTATUS_CONFIG = "${pkgs.subtitleStatusCfg}";
+              });
+        });
 }

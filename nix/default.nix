56ffeadbbbd57ddad88f1df1003c2938c88873f4
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-12-27 23:04:11 +0100

    Upgrade flake to use poetry2nix

--- nix/default.nix
+++ nix/default.nix
@@ -1,20 +1,6 @@
 final: prev:
 
-let
-  py3 = final.python3;
-  pyPkgs = py3.pkgs;
-  buildPythonPackage = py3.pkgs.buildPythonPackage;
-  fetchPypi = py3.pkgs.fetchPypi;
-  defaultArguments = {
-    inherit pyPkgs fetchPypi buildPythonPackage;
-  };
-in
 {
-  django-account = final.pkgs.callPackage ./django-account.nix defaultArguments;
-  django-bootstrap-form = final.pkgs.callPackage ./django-bootstrap-form.nix defaultArguments;
-  django-extensions = final.pkgs.callPackage ./django-extensions.nix defaultArguments;
-  python-kanboard-api = final.pkgs.callPackage ./python-kanboard-api.nix defaultArguments;
-
   subtitleStatusCfg = final.pkgs.writeText "subtitleStatus.cfg" ''
     [sql]
     type=postgresql

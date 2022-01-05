9fd79734b162d2f3f594d89852211756e58088e9
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-01-05 16:04:02 +0100

    Fix auth urls

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
-from account import views
+from django.conf.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login', views.login, name='login'),
-    url(r'^logout', views.logout, name='logout'),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- subtitleStatus/settings.py
+++ subtitleStatus/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'www',
     'django_extensions',
     'subtitleStatus',
+    'account',
 )
 
 MIDDLEWARE = (

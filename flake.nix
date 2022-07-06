5911f2a6776f0fa1469ac525f613d1738a1b650d
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-07-07 00:11:44 +0200

    Update flake
    - Move to nixpkgs-22.05
    - Prefer flake-utils-plus over flake-utils
    - Drop flake-compat
    - Prefer nix-direnv over lorri

--- flake.nix
+++ flake.nix
@@ -2,41 +2,54 @@
   description = "subtitlesStatus, the c3subtitles.de frontend code";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "utils/flake-utils";
+      };
     };
-    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlay = import ./nix;
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              overlay
-              poetry2nix.overlay
-            ];
-          };
-        in
-        {
-          inherit overlay;
-          devShell =
-            let
-              poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-                projectDir = ./.;
-                overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: { uwsgi = null; });
-              };
-            in
-            poetryEnv.env.overrideAttrs
-              (oldAttrs: {
-                SUBTITLESTATUS_CONFIG = "${pkgs.subtitleStatusCfg}";
-                buildInputs = [ pkgs.poetry ];
+  outputs = { self, nixpkgs, utils, poetry2nix, ... }@inputs:
+    let overlay = import ./nix;
+    in utils.lib.mkFlake {
+      inherit self inputs overlay;
+
+      sharedOverlays = [ poetry2nix.overlay overlay ];
+
+      outputsBuilder = channels: {
+        devShell = let
+          poetryEnv = channels.nixpkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            overrides = channels.nixpkgs.poetry2nix.overrides.withDefaults
+              (self: super: {
+                uwsgi = null;
+                cryptography = super.cryptography.overridePythonAttrs (old: {
+                  cargoDeps = channels.nixpkgs.rustPlatform.fetchCargoTarball {
+                    inherit (old) src;
+                    name = "${old.pname}-${old.version}";
+                    sourceRoot = "${old.pname}-${old.version}/src/rust/";
+                    sha256 =
+                      "sha256-f8r6QclTwkgK20CNe9i65ZOqvSUeDc4Emv6BFBhh1hI=";
+                  };
+                  cargoRoot = "src/rust";
+                  nativeBuildInputs = old.nativeBuildInputs
+                    ++ (with channels.nixpkgs.rustPlatform; [
+                      rust.rustc
+                      rust.cargo
+                      cargoSetupHook
+                    ]);
+                });
+
               });
+          };
+        in poetryEnv.env.overrideAttrs (oldAttrs: {
+          SUBTITLESTATUS_CONFIG = "${channels.nixpkgs.subtitleStatusCfg}";
+          buildInputs = [ channels.nixpkgs.poetry ];
         });
+      };
+    };
 }
